e506942348d836969bb0fc3af7e03667be51f00f
Tiago Costa <tiagodpv@gmail.com>
2022-07-19 16:03:05 -0400

    add backend

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,63 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lasttName;
+	
+	@Column(name="email_id")
+	private String emailId;
+		
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lasttName, String emailId) {
+		super();
+		this.firstName = firstName;
+		this.lasttName = lasttName;
+		this.emailId = emailId;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLasttName() {
+		return lasttName;
+	}
+	public void setLasttName(String lasttName) {
+		this.lasttName = lasttName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	
+}
